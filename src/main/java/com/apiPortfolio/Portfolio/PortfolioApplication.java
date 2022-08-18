e7f99cdaf956d0649f9dad400a41757fe1b83c9f
Piterpiojo <delgado.aldo87@gmail.com>
2022-08-18 19:00:27 -0300

    Con heroku y cloud

--- src/main/java/com/apiPortfolio/Portfolio/PortfolioApplication.java
+++ src/main/java/com/apiPortfolio/Portfolio/PortfolioApplication.java
@@ -2,12 +2,22 @@ package com.apiPortfolio.Portfolio;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class PortfolioApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PortfolioApplication.class, args);
+	}
+        public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/greeting-javaconfig").allowedOrigins("https://fierce-cliffs-56094.herokuapp.com/");
+			}
+		};
 	}
 
 }
